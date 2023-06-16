00f8e6ec64d11c4246e275d28c26e0e8573b68a7
Charles Agoe <agoetee@gmail.com>
2023-06-15 22:58:24 -0700

    display using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry to the program
+ * Return: always zero
+ */
+
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+
+	return (0);
+
+}
+
