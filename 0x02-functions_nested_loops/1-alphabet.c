065354425edc3ae6a328f165c1d12704b095021f
Charles Agoe <agoetee@gmail.com>
2023-06-20 15:42:38 -0700

    print the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabets
+ */
+
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+}
+
