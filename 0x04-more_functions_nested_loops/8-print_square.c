d64a710b9afeb31944407f5430a1e466cdacd3d0
Charles Agoe <agoetee@gmail.com>
2023-06-23 05:51:12 -0700

    print # of squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,20 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		if (size <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
 		{
+
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
-		for (j = 1; j <= size; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
