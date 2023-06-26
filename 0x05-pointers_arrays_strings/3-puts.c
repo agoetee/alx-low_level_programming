2b03d508c7b5ea82430f60e99b6d780c38185db1
Charles Agoe <agoetee@gmail.com>
2023-06-26 16:45:55 -0700

    output a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: the string to be printed
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
