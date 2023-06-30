48ef05816ec903b17b6612c119960defddff8aa4
Charles Agoe <agoetee@gmail.com>
2023-06-30 16:19:26 -0700

    reverse the content of array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,16 +8,14 @@
 void reverse_array(int *a, int n)
 {
 	int temp, i;
-	int end = n - 1;
 
 	i = 0;
-	while (i < end)
+	while (i < n--)
 	{
 		temp = a[i];
 		a[i] = a[n];
 		a[n] = temp;
 		i++;
-		n--;
 	}
 
 
