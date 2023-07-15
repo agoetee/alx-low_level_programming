74fe73941e1f528614292c5891e3b3ebd480f498
Charles Agoe <agoetee@gmail.com>
2023-07-15 06:53:25 -0700

    create an array of ints

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	length = (max - min) + 1;
-	mem = malloc(sizeof(int) * length);
+	mem = malloc(sizeof(*mem) * length);
 	if (mem == NULL)
 		return (NULL);
 	for (i = 0; i <= length; i++)
